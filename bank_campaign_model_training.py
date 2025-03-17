43782d420dcd0bec6b64bf0575e13213f82638f5
Vincent Yin <vincent_yin@hotmail.com>
2025-03-17 18:04:27 -0400

    Update bank_campaign_model_training.py

--- bank_campaign_model_training.py
+++ bank_campaign_model_training.py
@@ -1,4 +1,6 @@
 import pandas as pd
+
+
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
